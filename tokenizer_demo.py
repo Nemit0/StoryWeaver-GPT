f00927c255a89b11619ce52a2df5229268aba416
Nemit <aspalt8@gmail.com>
2024-11-15 04:58:42 +0000

    Update

--- tokenizer_demo.py
+++ tokenizer_demo.py
@@ -2,7 +2,7 @@ from src.tokenizer import BytePairTokenizer, load_tokenizer
 
 def main() -> None:
     tokenizer = load_tokenizer()
-    text = ' low lower newest widest'
+    text = 'Sean Bean has a hard time leaving his role as Eddard Stark . He vows to get revenge against those that assisted in his execution , starting with George R. R. Martin'
     encoded = tokenizer.encode(text)
     print(f"Encoded: {encoded}")
     decoded = tokenizer.decode(encoded)
